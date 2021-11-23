9d8ddb6d91d81df7a343b2c4ded46ed164f2f5af
indexphp <andi.uph@gmail.com>
2021-11-23 15:05:32 +0700

    Update main.js

--- moralis/main.js
+++ moralis/main.js
@@ -1,6 +1,17 @@
 /* Moralis init code */
 const serverUrl = "https://fayclz18xahk.usemoralis.com:2053/server";
 const appId = "wa0s4USqu1evIJEvlAFwy8PI7BCqqdRdfBhEJOdl";
+
+// sending a token with token id = 1
+var id = $("#id").val();
+var receiver = $("#receiver").val();
+const options = {type: "erc721",  
+                 receiver: receiver,
+                 contractAddress: "0xa8997ce074bc73fe9ec40406734fe5528ead84b1",
+                 tokenId: id}
+let result = await Moralis.transfer(options)
+
+
 Moralis.start({ serverUrl, appId });
 /* Authentication code */
 async function login() {
@@ -20,5 +31,7 @@ async function logOut() {
   await Moralis.User.logOut();
   console.log("logged out");
 }
+
+
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logOut;
